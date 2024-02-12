9f05ccb851b681cf86e925ba7b751588e1820b87
sdamyanov <stefan@e-motive.online>
2024-02-12 13:11:05 +0200

    Added graphql and storage

--- src/App.js
+++ src/App.js
@@ -56,8 +56,8 @@ const App = ({ signOut }) => {
     });
     fetchNotes();
     event.target.reset();
-  }
-  
+  }  
+
   async function deleteNote({ id, name }) {
     const newNotes = notes.filter((note) => note.id !== id);
     setNotes(newNotes);
@@ -90,10 +90,10 @@ const App = ({ signOut }) => {
             required
           />
           <View
-            name="image"
-            as="input"
-            type="file"
-            style={{ alignSelf: "end" }}
+          name="image"
+          as="input"
+          type="file"
+          style={{ alignSelf: "end" }}
           />
           <Button type="submit" variation="primary">
             Create Note
@@ -102,29 +102,29 @@ const App = ({ signOut }) => {
       </View>
       <Heading level={2}>Current Notes</Heading>
       <View margin="3rem 0">
-        {notes.map((note) => (
-          <Flex
-            key={note.id || note.name}
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Text as="strong" fontWeight={700}>
-              {note.name}
-            </Text>
-            <Text as="span">{note.description}</Text>
-            {note.image && (
-            <Image
-              src={note.image}
-              alt={`visual aid for ${notes.name}`}
-              style={{ width: 400 }}
-            />
-            )}
-            <Button variation="link" onClick={() => deleteNote(note)}>
-              Delete note
-            </Button>
-          </Flex>
-        ))}
+      {notes.map((note) => (
+  <Flex
+    key={note.id || note.name}
+    direction="row"
+    justifyContent="center"
+    alignItems="center"
+  >
+    <Text as="strong" fontWeight={700}>
+      {note.name}
+    </Text>
+    <Text as="span">{note.description}</Text>
+    {note.image && (
+      <Image
+        src={note.image}
+        alt={`visual aid for ${notes.name}`}
+        style={{ width: 400 }}
+      />
+    )}
+    <Button variation="link" onClick={() => deleteNote(note)}>
+      Delete note
+    </Button>
+  </Flex>
+))}
       </View>
       <Button onClick={signOut}>Sign Out</Button>
     </View>
